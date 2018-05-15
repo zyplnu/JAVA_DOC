bbf22d02b25e807c676fb2e09a3b668928485fc9
youngFF <597605057@qq.com>
2018-05-15 09:04:08 +0800

    log4j test done

--- log4jtest/src/logger/test/Log4jExample.java
+++ log4jtest/src/logger/test/Log4jExample.java
@@ -1,18 +1,25 @@
 package logger.test;
 
 import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.log4j.spi.RootLogger;
+import org.apache.log4j.PatternLayout;
 
+
+//
 public class Log4jExample {
 
 
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger(RootLogger.class);
-        logger.addAppender(new ConsoleAppender());
-        logger.debug("hello this is a debug messgae");
-        logger.info("hello this is an info message");
+        Logger rootLogger = Logger.getRootLogger();
+//        rootLogger.setLevel(Level.INFO);
+        rootLogger.addAppender(new ConsoleAppender(new PatternLayout("%-6r [%p] %c - %m%n")));
 
+
+        // get logger , 这条语句的作用就是将类Log4jExample配置成为一个Logger
+        Logger logger = Logger.getLogger(Log4jExample.class);
+        logger.info("info message");
+        logger.debug("debug message");
     }
 
 }
